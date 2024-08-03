cd70ccf997d4388823f1ac847e876987d65884f6
5nam <ohnam90@gmail.com>
2024-08-03 14:50:05 +0900

    init

---
+++ server/src/test/java/nior_near/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package nior_near.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
