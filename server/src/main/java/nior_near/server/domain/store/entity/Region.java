b558f5de9f9a0d3231d79fbd0f0747f4ed829442
5nam <ohnam90@gmail.com>
2024-08-08 22:13:38 +0900

    [feat(db)] : PossiblePickUp 관련 코드 제거

--- server/src/main/java/nior_near/server/domain/store/entity/Region.java
+++ server/src/main/java/nior_near/server/domain/store/entity/Region.java
@@ -1,11 +1,6 @@
 package nior_near.server.domain.store.entity;
 
 import jakarta.persistence.*;
-import nior_near.server.domain.order.entity.PossiblePickUp;
-import nior_near.server.global.util.Time;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
 public class Region {
@@ -18,7 +13,4 @@ public class Region {
     private String name;
 
     private Long upperId;
-
-    @OneToMany(mappedBy = "region", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<PossiblePickUp> possiblePickUpList = new ArrayList<>();
 }
