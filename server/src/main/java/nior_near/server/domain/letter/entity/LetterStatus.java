47c685b5d23598f43ddf2950b52af9851ec91469
nicehcy <nicehcy2@naver.com>
2024-08-10 18:54:45 +0900

    Fix: LetterStatus @Getter 어노테이션 추가

--- server/src/main/java/nior_near/server/domain/letter/entity/LetterStatus.java
+++ server/src/main/java/nior_near/server/domain/letter/entity/LetterStatus.java
@@ -1,8 +1,10 @@
 package nior_near.server.domain.letter.entity;
 
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
+@Getter
 public enum LetterStatus {
 
     READ("열람 완료"), UNREAD("미열람");
