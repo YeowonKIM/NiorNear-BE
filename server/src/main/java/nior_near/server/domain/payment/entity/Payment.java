0521569aa4cd209e43dc46cea852ec1ec62fa138
YeowonKIM <kkyoo152@gmail.com>
2024-08-13 18:16:02 +0900

    [Feat] Payment 관련 entity 생성

---
+++ server/src/main/java/nior_near/server/domain/payment/entity/Payment.java
@@ -0,0 +1,32 @@
+package nior_near.server.domain.payment.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Payment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long price;
+    private PaymentStatus paymentStatus;
+    private String paymentId;
+
+    @Builder
+    public Payment(Long price, PaymentStatus paymentStatus) {
+        this.price = price;
+        this.paymentStatus = paymentStatus;
+    }
+
+    public void changePaymentStatus(PaymentStatus paymentStatus, String paymentId) {
+        this.paymentStatus = paymentStatus;
+        this.paymentId = paymentId;
+    }
+}
