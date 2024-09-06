53e9edc01abf7a8c11accaad51a258116278dc80
5nam <ohnam90@gmail.com>
2024-09-07 00:21:24 +0900

    [refactor] : validation orderAddRequest 에 추가하여 에러 메시지 구체화

--- src/main/java/nior_near/server/domain/order/dto/request/OrderAddRequestDto.java
+++ src/main/java/nior_near/server/domain/order/dto/request/OrderAddRequestDto.java
@@ -1,5 +1,7 @@
 package nior_near.server.domain.order.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,15 +10,23 @@ import java.util.List;
 @Getter
 @Setter
 public class OrderAddRequestDto {
+
+    @NotBlank
     private Long storeId;
+    @NotNull
     private String requestMessage;
+    @NotBlank
     private String memberName;
+    @NotBlank
     private String memberPhone;
+    @NotBlank
     private List<OrderMenuItem> menus;
 
     @Getter @Setter
     public static class OrderMenuItem {
+        @NotBlank
         private Long menuId;
+        @NotBlank
         private Integer quantity;
     }
 }
